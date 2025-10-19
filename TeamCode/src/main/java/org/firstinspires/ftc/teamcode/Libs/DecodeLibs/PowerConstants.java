4376d69081e5479961b204ea041b14be86dcf94c
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2025-10-19 01:38:13 -0400

    Created an interface for easy access to power constants used in different mechanisms.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libs/DecodeLibs/PowerConstants.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Libs.DecodeLibs;
+
+public interface PowerConstants {
+	double INTAKE_IN_POWER = 0.8;
+	double INTAKE_OUT_POWER = -0.8;
+
+	double LAUNCH_POWER = 0.75;
+}
