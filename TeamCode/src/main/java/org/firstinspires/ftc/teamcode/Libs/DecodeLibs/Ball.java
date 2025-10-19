15e356e6b25ded77d73dc7714ab5d0726ac58a72
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2025-10-19 01:38:58 -0400

    Reformatted file.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libs/DecodeLibs/Ball.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libs/DecodeLibs/Ball.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.Libs.DecodeLibs;
 
+/** Contains info about artifact stored in magazine. */
 public class Ball {
+	private final boolean isGreen;
 
-    private boolean isGreen;
-    public Ball(boolean isGreen){
-        this.isGreen = isGreen;
-    }
+	public Ball(boolean isGreen){
+		this.isGreen = isGreen;
+	}
+
+	public boolean getIsGreen() { return isGreen; }
 }
