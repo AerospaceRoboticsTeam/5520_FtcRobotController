20f86dd75a29dce5cd777e2d59d65cfef0e31099
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2025-10-20 17:48:30 -0400

    Added some more boilerplate using the new part classes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Auto_Dev.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Auto_Dev.java
@@ -1,17 +1,37 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import org.firstinspires.ftc.teamcode.constants.Team;
+import org.firstinspires.ftc.teamcode.libs.decodeLibs.Intake;
+import org.firstinspires.ftc.teamcode.libs.decodeLibs.Launcher;
+import org.firstinspires.ftc.teamcode.apriltags.TagProcessor;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name = "Dev Auto", group = "Auto")
 public class Auto_Dev extends LinearOpMode {
-  @Override
+	private TagProcessor tagProcessor;
+	private Intake intake;
+	private Launcher launcher;
+
+	@Override
   public void runOpMode() throws InterruptedException {
-    // Wait for Op mode to start and cancel startup if stopped
+		intake = new Intake(this);
+		launcher = new Launcher(this);
+
+		// Configure state
+		tagProcessor = new TagProcessor(
+			this, gamepad1.a ? Team.RED : Team.BLUE
+		);
+
+		// Wait for Op mode to start and cancel startup if stopped
     waitForStart();
     if(isStopRequested()) return;
 
     // Run main loop
-    while(opModeIsActive() && !isStopRequested()) {}
+    while(opModeIsActive() && !isStopRequested()) {
+			// If the artifact pattern has not been found, attempt to get it
+			if(tagProcessor.artifactPattern == 0) tagProcessor.getArtifactPattern();
+		}
   }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp_Dev.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp_Dev.java
@@ -3,17 +3,24 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.libs.decodeLibs.Intake;
+import org.firstinspires.ftc.teamcode.libs.decodeLibs.Launcher;
 import org.firstinspires.ftc.teamcode.mecanumdrive.MecanumDrive;
 
 @TeleOp(name = "Dev TeleOp", group = "TeleOp")
 public class TeleOp_Dev extends LinearOpMode {
   private MecanumDrive mecanumDrive;
+	private Intake intake;
+	private Launcher launcher;
+
   private int artifactNum = 0;
 
   @Override
   public void runOpMode() throws InterruptedException {
     // Initialize mechanisms
     mecanumDrive = new MecanumDrive(this);
+		intake = new Intake(this);
+		launcher = new Launcher(this);
 
     // Wait for Op mode to start and cancel startup if stopped
     if(isStopRequested()) return;
@@ -24,18 +31,23 @@ public class TeleOp_Dev extends LinearOpMode {
       mecanumDrive.drive();
 
       // Activate the drivetrain's boost if the left trigger is pressed down
-      // NOTE: May want to change from != 0 to >= 0.5 to prevent accidental presses
-      if(gamepad1.left_trigger != 0) {
-        mecanumDrive.setBoost(1);
-      }
-      else {
-        mecanumDrive.setBoost(0.5);
-      }
+			if ((gamepad1.left_trigger >= 0.25)) mecanumDrive.setBoost(1);
+			else mecanumDrive.setBoost(0.5);
+
+			// Switch intake direction or turn it off
+			if(gamepad1.left_bumper) intake.intakeOut();
+			else if(gamepad1.right_bumper) intake.intakeOut();
+			else if(gamepad1.y) intake.intakeStop();
+
+			// Activate the launcher if the right trigger is pressed down
+			if(gamepad1.right_trigger >= 0.25) launcher.launch();
+			else launcher.stopLaunch();
 
       /* TODO: Add input handlers for controlling other hardware */
 
       // Get new telemetry data and push it to the driver station
       mecanumDrive.getTelemetryData();
+			launcher.getTelemetryData();
       telemetry.update();
     }
   }
