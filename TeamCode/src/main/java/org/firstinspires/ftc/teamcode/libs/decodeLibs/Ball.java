060c9c5fda980c88c2f79008c1c6e33fc5e85d13
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2025-10-20 17:50:36 -0400

    Added some documentation.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/decodeLibs/Ball.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/decodeLibs/Ball.java
@@ -1,12 +1,14 @@
-package org.firstinspires.ftc.teamcode.Libs.DecodeLibs;
+package org.firstinspires.ftc.teamcode.libs.decodeLibs;
 
 /** Contains info about artifact stored in magazine. */
 public class Ball {
 	private final boolean isGreen;
 
+	/** @param isGreen True if the ball is green, false if the ball is purple. */
 	public Ball(boolean isGreen){
 		this.isGreen = isGreen;
 	}
 
+	/** @return True if the ball is green, false if the ball is purple. */
 	public boolean getIsGreen() { return isGreen; }
 }
