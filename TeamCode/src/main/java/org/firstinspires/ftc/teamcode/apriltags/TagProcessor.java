6da4c8e87440913934ae0b07a11a6e13e0976979
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2025-10-24 23:20:25 -0400

    Removed the ApriltagProcessor and replaced it with Limelight. Not complete yet, but will be soon.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltags/TagProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltags/TagProcessor.java
@@ -1,21 +1,22 @@
 package org.firstinspires.ftc.teamcode.apriltags;
 
-import android.util.Size;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.IMU;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+import org.firstinspires.ftc.teamcode.constants.LimelightPipelines;
 import org.firstinspires.ftc.teamcode.constants.Team;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 
 /** The April tag processor for the Decode season. */
 public class TagProcessor {
 	private final LinearOpMode bot;
-	public final AprilTagProcessor tagProcessor;
-	public final VisionPortal visionPortal;
+	private final IMU imu;
+	private final Limelight3A limelight;
 
 	/** Stores the found artifact pattern. If one wasn't found, the value will be 0. */
 	private final Team team;
@@ -25,28 +26,34 @@ public class TagProcessor {
 		bot = opMode;
 		this.team = team;
 
-		tagProcessor = new AprilTagProcessor.Builder()
-			.setDrawAxes(true)
-			.setDrawCubeProjection(true)
-			.setDrawTagID(true)
-			.setDrawTagOutline(true)
-			.build();
+		// Initialize and set up IMU
+		RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
+		RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.UP;
+		RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);  // TODO: Change to match actual orientation
+		imu = bot.hardwareMap.get(IMU.class, "imu");
+		imu.initialize(new IMU.Parameters(orientationOnRobot));
 
-		visionPortal = new VisionPortal.Builder()
-			.addProcessor(tagProcessor)
-			.setCamera(bot.hardwareMap.get(WebcamName.class, "Webcam 1"))
-			.setCameraResolution(new Size(640, 480))
-			.build();
+		// Initialize Limelight
+		limelight = bot.hardwareMap.get(Limelight3A.class, "limelight");
+		limelight.setPollRateHz(100); // This makes the Limelight computer refresh 100 times per
+		limelight.pipelineSwitch(LimelightPipelines.APRILTAG_PIPELINE);
+	}
+
+	/** Tells the Limelight computer to start. */
+	public void startLimelight() { limelight.start(); }
+
+	/** Update the Limelight computer. */
+	public void update() {
+		YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+		limelight.updateRobotOrientation(orientation.getYaw(AngleUnit.DEGREES));
 	}
 
 	/** Finds the artifact pattern from the obelisk using the camera's output. */
 	public void getArtifactPattern() {
-		if(!tagProcessor.getDetections().isEmpty() && artifactPattern == 0) {
-			for(AprilTagDetection tag : tagProcessor.getDetections()) {
-				if(tag.id == TagIDs.GGP || tag.id == TagIDs.PGP || tag.id == TagIDs.PPG) {
-					artifactPattern = tag.id;
-				}
-			}
+		// If the artifact pattern has not been found, attempt to get it
+		if(artifactPattern == 0) {
+			LLResult result = limelight.getLatestResult();
+			if(result != null && result.isValid() && result.getPipelineType().equals("TODO: Get Apriltag type")) {}
 		}
 	}
 
@@ -54,17 +61,8 @@ public class TagProcessor {
 	 * @return An object containing position vectors to the middle of the goal tag relative to the
 	 * robot's current position if the goal tag is in sight, otherwise null.
 	 */
-	public AprilTagPoseFtc getVectorsToGoalTag() {
-		if(tagProcessor.getDetections().isEmpty()) return null;
+	public void getVectorsToGoalTag() {
 
-		AprilTagPoseFtc tagPose = null;
-
-		for(AprilTagDetection tag : tagProcessor.getDetections()) {
-			if(team == Team.BLUE && tagPose == null && tag.id == TagIDs.BLUE_TAG) tagPose = tag.ftcPose;
-			else if(team == Team.RED && tagPose == null && tag.id == TagIDs.RED_TAG) tagPose = tag.ftcPose;
-		}
-
-		return tagPose;
 	}
 
 	public void getTelemetryData() {
