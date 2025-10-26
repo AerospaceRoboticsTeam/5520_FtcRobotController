3918f2c52157b80cb0ca598b78ca11faa43a588c
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2025-10-25 21:07:57 -0400

    Mostly finished the getVectorsToGoalTag method, just need to check if the Pose3D contains the right vectors.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltags/TagProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltags/TagProcessor.java
@@ -39,7 +39,7 @@ public class TagProcessor {
 
 		// Initialize Limelight
 		limelight = bot.hardwareMap.get(Limelight3A.class, "limelight");
-		limelight.setPollRateHz(100); // This makes the Limelight computer refresh 100 times per
+		limelight.setPollRateHz(100); // This makes the Limelight computer refresh 100 times per second
 		limelight.pipelineSwitch(LimelightPipelines.APRILTAG_PIPELINE);
 	}
 
@@ -52,16 +52,26 @@ public class TagProcessor {
 		limelight.updateRobotOrientation(orientation.getYaw(AngleUnit.DEGREES));
 	}
 
+	/** Checks whether the {@link LLResult} is a valid April Tag result. */
+	private boolean isValidAprilTagResult(LLResult result) {
+		return result == null || !result.isValid() || !result.getPipelineType().equals("tagType"); // TODO: Get Apriltag type
+	}
+
+	/** Checks whether there are any April Tag results available. */
+	private boolean tagResultsExist(List<LLResultTypes.FiducialResult> tagResults) {
+		return tagResults == null || tagResults.isEmpty();
+	}
+
 	/** Finds the artifact pattern from the obelisk using the camera's output. */
 	public void getArtifactPattern() {
 		// If the artifact pattern has not been found, attempt to get it
 		if(artifactPattern != 0) return;
 
 		LLResult result = limelight.getLatestResult();
-		if(result == null || !result.isValid() || !result.getPipelineType().equals("tagType")) return; // TODO: Get Apriltag type
+		if(isValidAprilTagResult(result)) return;
 
 		List<LLResultTypes.FiducialResult> tags = result.getFiducialResults();
-		if(tags == null || tags.isEmpty()) return;
+		if(tagResultsExist(tags)) return;
 
 		// Check the IDs of the found April Tags, setting the artifact pattern if an ID matches a pattern ID
 		for(LLResultTypes.FiducialResult tag : tags) {
@@ -71,11 +81,23 @@ public class TagProcessor {
 	}
 
 	/**
-	 * @return An object containing position vectors to the middle of the goal tag relative to the
-	 * robot's current position if the goal tag is in sight, otherwise null.
+	 * @return A {@link Pose3D} containing vectors to the middle of the goal tag relative
+	 * to the robot's current position if the goal tag is in sight, otherwise null.
 	 */
-	public void getVectorsToGoalTag() {
+	public Pose3D getVectorsToGoalTag() {
+		LLResult result = limelight.getLatestResult();
+		if(isValidAprilTagResult(result)) return null;
 
+		List<LLResultTypes.FiducialResult> tags = result.getFiducialResults();
+		if(tagResultsExist(tags)) return null;
+
+		for(LLResultTypes.FiducialResult tag : tags) {
+			int tagID = tag.getFiducialId();
+			if(team == Team.BLUE && tagID == TagIDs.BLUE_TAG) return tag.getRobotPoseTargetSpace();
+			else if(team == Team.RED && tagID == TagIDs.RED_TAG) return tag.getRobotPoseTargetSpace();
+		}
+
+		return null;
 	}
 
 	public void getTelemetryData() {
