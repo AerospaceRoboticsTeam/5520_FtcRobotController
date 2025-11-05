6fa10d67ee451e6ff169fa59b992b61a92ddda33
Gabriel Woodruff <gabriel.o.woodruff@zisostudios.com>
2025-11-04 19:30:02 -0500

    Renamed motors and started making linearVelocityToPower method.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/decodeLibs/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/decodeLibs/Launcher.java
@@ -4,36 +4,41 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.constants.ArtifactNumRef;
-import org.firstinspires.ftc.teamcode.constants.PowerConstants;
 
 /** Controller for the robot's launcher. */
 public class Launcher {
 	private final LinearOpMode bot;
-	private final DcMotor launcherMotorL;
-	private final DcMotor launcherMotorR;
+	private final DcMotor launcherMotorLeft;
+	private final DcMotor launcherMotorRight;
 
 	private LauncherStatus state;
 	private final ArtifactNumRef artifactNum = new ArtifactNumRef(0);
 
 	public Launcher(LinearOpMode opMode) {
 		bot = opMode;
-		launcherMotorL = bot.hardwareMap.get(DcMotor.class, "launcherMotorL");
-		launcherMotorL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-		launcherMotorR = bot.hardwareMap.get(DcMotor.class, "launcherMotorR");
-		launcherMotorR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-		state = LauncherStatus.WAITING;
+		launcherMotorLeft = bot.hardwareMap.get(DcMotor.class, "launcherMotorLeft");
+		launcherMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+		launcherMotorRight = bot.hardwareMap.get(DcMotor.class, "launcherMotorRight");
+		launcherMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+		state = LauncherStatus.IDLE;
 	}
 
-	public void launch(double motorPowerL, double motorPowerR) {
-		launcherMotorR.setPower(motorPowerR);
-		launcherMotorL.setPower(motorPowerL);
+	public void launch(double motorPowerLeft, double motorPowerRight) {
+		launcherMotorLeft.setPower(motorPowerLeft);
+		launcherMotorRight.setPower(motorPowerRight);
 		state = LauncherStatus.LAUNCHING;
 	}
 
 	public void stopLaunch() {
-		launcherMotorR.setPower(0);
-		launcherMotorL.setPower(0);
-		state = LauncherStatus.WAITING;
+		launcherMotorRight.setPower(0);
+		launcherMotorLeft.setPower(0);
+		state = LauncherStatus.IDLE;
+	}
+
+	public int[] linearVelocityToPower(float leftMotorVelocity, float rightMotorVelocity) {
+
+
+		return new int[]{};
 	}
 
 	public void getTelemetryData() {
@@ -42,7 +47,7 @@ public class Launcher {
 }
 
 enum LauncherStatus {
-	WAITING,
+	IDLE,
 	LAUNCHING,
 	READY
 }
