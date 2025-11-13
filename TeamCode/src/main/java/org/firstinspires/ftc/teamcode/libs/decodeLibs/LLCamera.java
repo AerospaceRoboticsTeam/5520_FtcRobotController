32e7ea8bc0de9ab26501116bd86ac58f8d19bfb9
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2025-11-12 22:29:07 -0500

    Added a reference to the TagProcessor instance for future use.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/decodeLibs/LLCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/decodeLibs/LLCamera.java
@@ -3,11 +3,15 @@ package org.firstinspires.ftc.teamcode.libs.decodeLibs;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.apriltags.TagProcessor;
+
+/** Controller for the Limelight itself as well as the servo it's attached to. */
 public class LLCamera {
 	private final Servo cameraServo;
+	private final TagProcessor tagProcessor;
 
-	public LLCamera(LinearOpMode opMode) {
+	public LLCamera(LinearOpMode opMode, TagProcessor tagProcessor) {
 		cameraServo = opMode.hardwareMap.get(Servo.class, "limeLightCameraServo");
-
+		this.tagProcessor = tagProcessor;
 	}
 }
