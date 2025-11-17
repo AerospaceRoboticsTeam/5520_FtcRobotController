23f8fc5ee176a81df8f644503ac653a9ed475fbe
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2025-11-17 10:16:25 -0500

    Added a few fields and methods.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/decodeLibs/LLCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/decodeLibs/LLCamera.java
@@ -1,18 +1,28 @@
 package org.firstinspires.ftc.teamcode.libs.decodeLibs;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 import org.firstinspires.ftc.teamcode.apriltags.TagProcessor;
 
 /** Controller for the Limelight itself as well as the servo it's attached to. */
-//TODO: Update based on Pipelines, as outlined in the Programming Notes.
+@Configurable
 public class LLCamera {
-	private final Servo cameraServo;
+	private final ServoImplEx cameraServo;
 	private final TagProcessor tagProcessor;
 
+	private double cameraAngle;
+
+	private static final double rotationRate = 1.0; // The degrees per second the limelight should rotate
+	private double lastAngle;
+
 	public LLCamera(LinearOpMode opMode, TagProcessor tagProcessor) {
-		cameraServo = opMode.hardwareMap.get(Servo.class, "limeLightCameraServo");
+		cameraServo = opMode.hardwareMap.get(ServoImplEx.class, "limeLightCameraServo");
 		this.tagProcessor = tagProcessor;
+
+		cameraAngle = 0;
 	}
+
+	public void spin() {}
 }
