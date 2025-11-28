29c9d11bbbb352a517a26f4f6eb7981e54da81c0
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2025-11-28 16:16:05 -0500

    Started making class for intake sensors.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/decodeLibs/IntakeSensors.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.libs.decodeLibs;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+
+public class IntakeSensors {
+	private NormalizedColorSensor colorSensor;
+
+	public IntakeSensors(OpMode opMode) {
+		colorSensor = opMode.hardwareMap.get(NormalizedColorSensor.class, "intakeColorSensor");
+	}
+
+	public SensorColor getDetectedColor() {
+		NormalizedRGBA colors = colorSensor.getNormalizedColors();
+
+
+	}
+}
