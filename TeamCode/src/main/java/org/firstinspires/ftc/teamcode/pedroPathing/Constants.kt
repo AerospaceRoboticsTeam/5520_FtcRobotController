05c9dcfa4348d31b024325008bcd3a1cc0e80bae
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2026-01-14 13:54:52 -0500

    Converted to Kotlin.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.kt
@@ -1,75 +1,74 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
+package org.firstinspires.ftc.teamcode.pedroPathing
 
-import com.pedropathing.follower.FollowerConstants;
-import com.pedropathing.ftc.drivetrains.MecanumConstants;
-import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.follower.Follower
+import com.pedropathing.follower.FollowerConstants
+import com.pedropathing.ftc.FollowerBuilder
+import com.pedropathing.ftc.drivetrains.MecanumConstants
+import com.pedropathing.ftc.localization.constants.PinpointConstants
+import com.pedropathing.paths.PathConstraints
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver
+import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
+import org.firstinspires.ftc.teamcode.subsystems.drivetrain.DrivetrainConstants
 
-import com.pedropathing.follower.Follower;
-import com.pedropathing.ftc.FollowerBuilder;
-import com.pedropathing.paths.PathConstraints;
-import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+object Constants {
+  /**
+   * Consist of values from the automatic, PID, and centripetal tuners.
+   * <p>For more details, see <a href="https://pedropathing.com/docs/pathing/constants">
+   * https://pedropathing.com/docs/pathing/constants</a>.
+   */
+  val followerConstants: FollowerConstants = FollowerConstants()
+    .mass(14.5) // TODO: Set this to the robot's weight when building is complete in KG
+    .useSecondaryTranslationalPIDF(true)
+    .useSecondaryHeadingPIDF(true)
+    .useSecondaryDrivePIDF(true);
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.subsystems.drivetrain.DrivetrainConstants;
+  /**
+   * Contain constants specific to your drivetrain type. For example, mecanum drivetrain
+   * constants contain the motor names.
+   * <p>For more details, see <a href="https://pedropathing.com/docs/pathing/constants">
+   * https://pedropathing.com/docs/pathing/constants</a>.
+   */
+  val driveConstants: MecanumConstants = MecanumConstants()
+    .maxPower(1.0) // NOTE: Must be a value between 0 and 1
+    .leftFrontMotorName(DrivetrainConstants.LEFT_FRONT_MOTOR)
+    .leftRearMotorName(DrivetrainConstants.LEFT_BACK_MOTOR)
+    .rightFrontMotorName(DrivetrainConstants.RIGHT_FRONT_MOTOR)
+    .rightRearMotorName(DrivetrainConstants.RIGHT_BACK_MOTOR)
+    .leftFrontMotorDirection(DrivetrainConstants.LFM_DIRECTION)
+    .leftRearMotorDirection(DrivetrainConstants.LBM_DIRECTION)
+    .rightFrontMotorDirection(DrivetrainConstants.RFM_DIRECTION)
+    .rightRearMotorDirection(DrivetrainConstants.RBM_DIRECTION)
+    .useBrakeModeInTeleOp(true);
 
-public class Constants {
-	/**
-	 * Consist of values from the automatic, PID, and centripetal tuners.
-	 * <p>For more details, see <a href="https://pedropathing.com/docs/pathing/constants">
-	 * https://pedropathing.com/docs/pathing/constants</a>.
-	 */
-	public static FollowerConstants followerConstants = new FollowerConstants()
-		.mass(14.5) // TODO: Set this to the robot's weight when building is complete in KG
-		.useSecondaryTranslationalPIDF(true)
-		.useSecondaryHeadingPIDF(true)
-		.useSecondaryDrivePIDF(true);
+  /**
+   * Contain constants specific to your localizer. For example, OTOS constants include
+   * the hardware map name of the OTOS and the offset.
+   * <p>For more details, see <a href="https://pedropathing.com/docs/pathing/constants">
+   * https://pedropathing.com/docs/pathing/constants</a>.
+   */
+  val localizerConstants: PinpointConstants = PinpointConstants()
+    .strafePodX(0.5)
+    .forwardPodY(-5.0)
+    .distanceUnit(DistanceUnit.CM)
+    .hardwareMapName("pinpoint")
+    .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+    .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+    .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
 
-	/**
-	 * Contain constants specific to your drivetrain type. For example, mecanum drivetrain
-	 * constants contain the motor names.
-	 * <p>For more details, see <a href="https://pedropathing.com/docs/pathing/constants">
-	 * https://pedropathing.com/docs/pathing/constants</a>.
-	 */
-	public static MecanumConstants driveConstants = new MecanumConstants()
-		.maxPower(1) // NOTE: Must be a value between 0 and 1
-		.leftFrontMotorName(DrivetrainConstants.LEFT_FRONT_MOTOR)
-		.leftRearMotorName(DrivetrainConstants.LEFT_BACK_MOTOR)
-		.rightFrontMotorName(DrivetrainConstants.RIGHT_FRONT_MOTOR)
-		.rightRearMotorName(DrivetrainConstants.RIGHT_BACK_MOTOR)
-		.leftFrontMotorDirection(DrivetrainConstants.LFM_DIRECTION)
-		.leftRearMotorDirection(DrivetrainConstants.LBM_DIRECTION)
-		.rightFrontMotorDirection(DrivetrainConstants.RFM_DIRECTION)
-		.rightRearMotorDirection(DrivetrainConstants.RBM_DIRECTION)
-		.useBrakeModeInTeleOp(true);
+  /**
+   * Determine under what conditions a path may end.
+   * <p>For more details, see <a href="https://pedropathing.com/docs/pathing/constants">
+   * https://pedropathing.com/docs/pathing/constants</a>.
+   */
+  val pathConstraints: PathConstraints = PathConstraints(0.99, 100.0, 1.0, 1.0);
 
-	/**
-	 * Contain constants specific to your localizer. For example, OTOS constants include
-	 * the hardware map name of the OTOS and the offset.
-	 * <p>For more details, see <a href="https://pedropathing.com/docs/pathing/constants">
-	 * https://pedropathing.com/docs/pathing/constants</a>.
-	 */
-	public static PinpointConstants localizerConstants = new PinpointConstants()
-		.strafePodX(0.5)
-		.forwardPodY(-5)
-		.distanceUnit(DistanceUnit.CM)
-		.hardwareMapName("pinpoint")
-		.encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
-		.forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
-		.strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
-
-	/**
-	 * Determine under what conditions a path may end.
-	 * <p>For more details, see <a href="https://pedropathing.com/docs/pathing/constants">
-	 * https://pedropathing.com/docs/pathing/constants</a>.
-	 */
-	public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
-
-	public static Follower createFollower(HardwareMap hardwareMap) {
-		return new FollowerBuilder(followerConstants, hardwareMap)
-			.mecanumDrivetrain(driveConstants)
-			.pinpointLocalizer(localizerConstants)
-			.pathConstraints(pathConstraints)
-			.build();
-	}
+  @JvmStatic
+  fun createFollower(hardwareMap: HardwareMap): Follower {
+    return FollowerBuilder(followerConstants, hardwareMap)
+      .mecanumDrivetrain(driveConstants)
+      .pinpointLocalizer(localizerConstants)
+      .pathConstraints(pathConstraints)
+      .build();
+  }
 }
