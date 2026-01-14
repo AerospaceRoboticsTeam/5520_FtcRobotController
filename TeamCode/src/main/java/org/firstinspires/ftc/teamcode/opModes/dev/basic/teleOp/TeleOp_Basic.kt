09eaefd56af5b53e59e22a177b1e0416a666abe5
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2026-01-13 19:10:31 -0500

    Converted to Kotlin.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/dev/basic/teleOp/TeleOp_Basic.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/dev/basic/teleOp/TeleOp_Basic.kt
@@ -1,36 +1,40 @@
-package org.firstinspires.ftc.teamcode.opModes.dev.basic.teleOp;
+package org.firstinspires.ftc.teamcode.opModes.dev.basic.teleOp
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.opModes.OpModeGroups;
-import org.firstinspires.ftc.teamcode.subsystems.drivetrain.RudimentaryMecanumDrive;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.opModes.OpModeGroups
+import org.firstinspires.ftc.teamcode.subsystems.drivetrain.RudimentaryMecanumDrive
+import org.firstinspires.ftc.teamcode.utils.components.OpModeBase
 
 @TeleOp(name = "Basic Mecanum", group = OpModeGroups.BASIC)
-public class TeleOp_Basic extends LinearOpMode {
-	private RudimentaryMecanumDrive mecanumDrive;
+class TeleOp_Basic : LinearOpMode(), OpModeBase {
+  private lateinit var mecanumDrive: RudimentaryMecanumDrive;
 
-	@Override
-	public void runOpMode() throws InterruptedException {
-		// Initialize mechanisms
-		mecanumDrive = new RudimentaryMecanumDrive(this);
+  @Throws(InterruptedException::class)
+  override fun runOpMode() {
+    // Initialize mechanisms
+    mecanumDrive = RudimentaryMecanumDrive(this);
 
-		// Wait for Op mode to start and cancel startup if stopped
-		waitForStart();
-		if(isStopRequested()) return;
+    // Wait for Op mode to start and cancel startup if stopped
+    waitForStart()
+    if(isStopRequested) return;
 
-		// Run main loop
-		while(opModeIsActive() && !isStopRequested()) {
-			// Updates the drivetrain with the game controller's current values once every loop
-			mecanumDrive.drive();
+    // Run main loop
+    while(opModeIsActive() && !isStopRequested) {
+      // Updates the drivetrain with the game controller's current values once every loop
+      mecanumDrive.update();
 
-			// Activate the drivetrain's boost if the left trigger is pressed down
-			if(gamepad1.left_trigger >= 0.25) mecanumDrive.setBoost(1);
-			else mecanumDrive.setBoost(0.5);
+      // Activate the drivetrain's boost if the left trigger is pressed down
+      if(gamepad1.left_trigger >= 0.25) mecanumDrive.boost = 1.0;
+      else mecanumDrive.boost = 0.5;
 
-			// Get new telemetry data and push it to the driver station
-			mecanumDrive.getTelemetryData();
-			telemetry.update();
-		}
-	}
+      updateTelemetryData();
+    }
+  }
+
+  override fun updateTelemetryData() {
+    // Get new telemetry data and push it to the driver station
+    mecanumDrive.getTelemetryData();
+    telemetry.update();
+  }
 }
