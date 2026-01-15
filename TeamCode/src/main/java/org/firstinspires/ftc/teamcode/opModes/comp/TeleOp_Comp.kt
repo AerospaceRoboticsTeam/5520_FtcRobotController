8be675864b6c0322704b89cbdbcb286b910912c1
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2026-01-15 18:09:24 -0500

    Started making the competition op mode, which contains all of the basic variants of every subsystem. (Note: Currently experiencing issues with PedroPathing Mecanum Drive.)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/comp/TeleOp_Comp.kt
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.opModes.comp
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.opModes.OpModeGroups
+import org.firstinspires.ftc.teamcode.season.Intake
+import org.firstinspires.ftc.teamcode.season.LauncherBasic
+import org.firstinspires.ftc.teamcode.season.Magazine
+import org.firstinspires.ftc.teamcode.subsystems.drivetrain.PPMecanumDrive
+import org.firstinspires.ftc.teamcode.utils.components.OpModeBase
+
+@TeleOp(name = "Competition Complete", group = OpModeGroups.COMPETITION)
+class TeleOp_Comp : OpMode(), OpModeBase {
+  private lateinit var mecanumDrive: PPMecanumDrive;
+  private lateinit var intake: Intake;
+  private lateinit var magazine: Magazine;
+  private lateinit var launcher: LauncherBasic;
+
+  override fun init() {
+    mecanumDrive = PPMecanumDrive(this);
+    intake = Intake(this);
+    magazine = Magazine(this);
+    launcher = LauncherBasic(this);
+  }
+
+  override fun start() {
+    mecanumDrive.start();
+  }
+
+  override fun loop() {
+    mecanumDrive.update();
+    intake.update();
+    magazine.update();
+    launcher.update();
+
+    updateTelemetryData();
+  }
+
+  override fun updateTelemetryData() {
+    mecanumDrive.getTelemetryData();
+    intake.getTelemetryData();
+    magazine.getTelemetryData();
+    launcher.getTelemetryData();
+    telemetry.update();
+  }
+}
