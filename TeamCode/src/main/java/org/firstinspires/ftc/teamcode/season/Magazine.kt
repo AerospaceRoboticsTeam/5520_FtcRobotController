7f69e3e4bc13841d05f86a5ff53e0c64345cad20
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2026-01-15 18:03:16 -0500

    Added methods to control both servos individually and added methods to move artifacts down magazine.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/season/Magazine.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/season/Magazine.kt
@@ -1,33 +1,82 @@
 package org.firstinspires.ftc.teamcode.season
 
+import com.bylazar.configurables.annotations.Configurable
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 
 import com.qualcomm.robotcore.hardware.CRServo
+import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction
 import org.firstinspires.ftc.teamcode.utils.components.Subsystem
 
-class Magazine(private val bot: OpMode) : Subsystem {
+@Configurable
+class Magazine(private val opMode: OpMode) : Subsystem {
   companion object {
-    const val POWER = 0.5;
+    var POWER = 0.5;
+
+    var UPPER_SERVO_DIRECTION = Direction.FORWARD;
+    var LOWER_SERVO_DIRECTION = Direction.REVERSE;
   }
 
   /** Super Speed - Max 230 RPM. */
-  private val upperServo: CRServo = bot.hardwareMap.get(
+  private val upperServo: CRServo = opMode.hardwareMap.get(
     CRServo::class.java, "upperMagMotor"
   );
   /** Speed - Max 115 RPM. */
-  private val lowerServo: CRServo = bot.hardwareMap.get(
+  private val lowerServo: CRServo = opMode.hardwareMap.get(
     CRServo::class.java, "lowerMagMotor"
   );
-  private val gamepad = bot.gamepad2;
+  private val gamepad = opMode.gamepad2;
 
-  override fun update() {
-    if(gamepad.cross) move();
-    else stop();
+  init {
+    upperServo.direction = UPPER_SERVO_DIRECTION;
+    lowerServo.direction = LOWER_SERVO_DIRECTION;
   }
 
-  fun move() {
+  override fun update() {
+    // Move both servos
+    if(gamepad.circle) moveUp();
+    else if(gamepad.cross) moveDown();
+    else if(
+      !gamepad.dpad_up &&
+      !gamepad.dpad_down &&
+      !gamepad.dpad_left &&
+      !gamepad.dpad_right
+    ) stop();
+
+    // Move upper servo
+    if(gamepad.dpad_up) moveUpUpper();
+    else if(gamepad.dpad_right) moveDownUpper();
+    else stopUpper();
+
+    // Move lower servo
+    if(gamepad.dpad_down) moveUpLower();
+    else if(gamepad.dpad_left) moveDownLower();
+    else stopLower();
+  }
+
+  fun moveUp() {
     upperServo.power = POWER / 2;
     lowerServo.power = POWER;
+  }
+
+  fun moveDown() {
+    upperServo.power = -POWER / 2;
+    lowerServo.power = -POWER;
+  }
+
+  fun moveUpUpper() {
+    upperServo.power = POWER / 2;
+  }
+
+  fun moveUpLower() {
+    lowerServo.power = POWER;
+  }
+
+  fun moveDownUpper() {
+    upperServo.power = -POWER / 2;
+  }
+
+  fun moveDownLower() {
+    lowerServo.power = -POWER;
   }
 
   fun stop() {
@@ -35,8 +84,16 @@ class Magazine(private val bot: OpMode) : Subsystem {
     lowerServo.power = 0.0;
   }
 
+  fun stopUpper() {
+    upperServo.power = 0.0;
+  }
+
+  fun stopLower() {
+    lowerServo.power = 0.0;
+  }
+
   override fun getTelemetryData() {
-    bot.telemetry.addData("Upper Mag Servo Power", upperServo.power);
-    bot.telemetry.addData("Lower Mag Servo Power", lowerServo.power);
+    opMode.telemetry.addData("Upper Mag Servo Power", upperServo.power);
+    opMode.telemetry.addData("Lower Mag Servo Power", lowerServo.power);
   }
 }
