9da62430e5adf87bbfc127211598f647695bdb0a
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2026-01-16 18:11:55 -0500

    Added a new op mode to test the odometry pods.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/tests/teleOp/TeleOp_OdometryTest.kt
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.opModes.tests.teleOp
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
+import org.firstinspires.ftc.teamcode.libs.goBilda.GoBildaPinpointDriver
+import org.firstinspires.ftc.teamcode.utils.components.OpModeBase
+
+class TeleOp_OdometryTest : OpMode(), OpModeBase {
+  private lateinit var pinpoint: GoBildaPinpointDriver;
+
+  override fun init() {
+    pinpoint = hardwareMap.get(GoBildaPinpointDriver::class.java, "pinpoint");
+    pinpoint.setOffsets(0.5, -5.0, DistanceUnit.METER);
+  }
+
+  override fun loop() {
+    pinpoint.update();
+  }
+
+  override fun updateTelemetryData() {
+    telemetry.addData("X Pos",  "${pinpoint.getPosX(DistanceUnit.CM)} CM");
+    telemetry.addData("Y Pos", "${pinpoint.getPosY(DistanceUnit.CM)} CM");
+    telemetry.addData("Heading", "${pinpoint.getHeading(AngleUnit.DEGREES)} DEG");
+    telemetry.update();
+  }
+}
