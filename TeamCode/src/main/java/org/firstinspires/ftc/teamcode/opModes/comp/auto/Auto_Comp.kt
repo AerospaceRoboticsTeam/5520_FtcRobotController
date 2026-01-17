0f8b7ae6b6785e1899c01ae9c3be30f54ea5b095
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2026-01-16 19:23:58 -0500

    Make the robot move forward 1 meter.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/comp/auto/Auto_Comp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/comp/auto/Auto_Comp.kt
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode.opModes.comp.auto
 
 import com.pedropathing.follower.Follower
+import com.pedropathing.geometry.BezierLine
+import com.pedropathing.geometry.Pose
+import com.pedropathing.paths.Path
+import com.qualcomm.hardware.bosch.BNO055IMU
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import org.firstinspires.ftc.teamcode.opModes.OpModeGroups
@@ -25,17 +29,29 @@ class Auto_Comp : OpMode(), OpModeBase {
     intake = Intake(this);
     magazine = Magazine(this);
     launcher = LauncherBasic(this, lightController);
+
+    follower.setStartingPose(Pose(0.0, 0.0, 0.0));
   }
 
   override fun start() {
     follower.update();
+
+    val forwardPath = Path(BezierLine(
+      Pose(0.0, 0.0, 0.0),
+      Pose(39.37, 0.0, 0.0)
+    ));
+
+    follower.followPath(forwardPath);
   }
 
   override fun loop() {
-    TODO("Not yet implemented")
+    follower.update();
   }
 
   override fun updateTelemetryData() {
+    telemetry.addData("X Pos", follower.pose.x);
+    telemetry.addData("Y Pos", follower.pose.y);
+    telemetry.addData("Heading", Math.toDegrees(follower.pose.heading));
     lightController.getTelemetryData();
     intake.getTelemetryData();
     magazine.getTelemetryData();
