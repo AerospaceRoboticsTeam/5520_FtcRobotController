ef71c743120669f359cab4f0d46d6b23602cc6af
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2026-01-21 20:40:07 -0500

    Added the real pipeline numbers.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/vision/limelight/LimelightPipelines.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/vision/limelight/LimelightPipelines.kt
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.subsystems.vision.limelight
 
 object LimelightPipelines {
-  const val APRILTAG_PIPELINE = 0;
+  const val MOTIF = 0;
+  const val BLUE_GOAL = 1;
+  const val RED_GOAL = 2;
 }
