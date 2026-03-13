f7f0ea57ce47ce11d3233e5139b3d55e59c8c05b
Gabriel Woodruff <gabriel.o.woodruff@outlook.com>
2026-03-13 00:07:30 -0400

    Made a basic event system.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/events/EventBus.kt
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.events
+
+import kotlin.reflect.KClass
+
+/** Manages events. Used to register or dispatch events. */
+object EventBus {
+  @PublishedApi
+  internal val listeners = HashMap<KClass<out Event>, MutableList<(Event) -> Unit>>();
+
+  inline fun <reified T : Event> addEventListener(noinline listener: (T) -> Unit) {
+    val eventType = T::class;
+    val wrapped: (Event) -> Unit = { event -> listener(event as T) };
+
+    listeners.getOrPut(eventType) { mutableListOf() }.add(wrapped);
+  }
+
+  fun dispatch(event: Event) {
+    val eventType = event::class;
+
+    listeners[eventType]?.forEach { listener -> listener(event) };
+  }
+}
